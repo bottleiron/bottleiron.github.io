da8fed8619033be134787352f2189a8e8e74b53b
bottleiron <bottleiron@plantynet.com>
2020-11-05 10:17:23 +0900

    PUSH 테스트

--- sw.js
+++ sw.js
@@ -23,3 +23,49 @@ self.addEventListener('fetch', function(e) {
     })
   );
 });
+
+//푸시처리부분
+
+//Push Message 수신 이벤트
+self.addEventListener('push', function (event)
+{
+    console.log('[ServiceWorker] 푸시알림 수신: ', event);
+ 
+    //Push 정보 조회
+    var title = event.data.title || 'push';
+    var body = event.data.body;
+    var icon = event.data.icon || 'images/hello-icon-512.png'; //512x512
+    var badge = event.data.badge || 'images/hello-icon-128.png'; //128x128
+    var options = {
+        body: body,
+        icon: icon,
+        badge: badge
+    };
+ 
+    //Notification 출력
+    event.waitUntil(self.registration.showNotification(title, options));
+});
+ 
+//사용자가 Notification을 클릭했을 때
+self.addEventListener('notificationclick', function (event)
+{
+    console.log('[ServiceWorker] 푸시알림 클릭: ', event);
+ 
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({ type: "window" })
+            .then(function (clientList)
+            {
+                //실행된 브라우저가 있으면 Focus
+                for (var i = 0; i < clientList.length; i++)
+                {
+                    var client = clientList[i];
+                    if (client.url == '/' && 'focus' in client)
+                        return client.focus();
+                }
+                //실행된 브라우저가 없으면 Open
+                if (clients.openWindow)
+                    return clients.openWindow('https://bottleiron.github.io');
+            })
+    );
+});
