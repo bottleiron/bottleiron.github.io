b58efee0fe0c3704b73736323e34bb80a39e9d2b
bottleiron <bottleiron@plantynet.com>
2020-11-05 16:41:59 +0900

    push  Listener 로그기록

--- sw.js
+++ sw.js
@@ -31,8 +31,8 @@ self.addEventListener('push', function (event)
 {
     console.log('[ServiceWorker] 푸시알림 수신: ', event);
 
-    console.log(event.data)
-    console.log(event.notification)
+    console.log(event.data.json())
+    console.log(event.data.text())
     //Push 정보 조회
     var title = event.data.title || 'test';
     var body = event.data.body;
